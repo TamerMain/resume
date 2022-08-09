d0fa5f49d4a7e3414b07df2d782da27477d09553
churuya <arthur.tamer.main@gmail.com>
2022-08-09 17:05:03 +0300

    Updates

---
+++ static/js/600.1a62996f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkresume=self.webpackChunkresume||[]).push([[600],{600:function(e,n,s){s.r(n);var u=s(184);n.default=function(){return(0,u.jsx)("div",{className:"centered",children:(0,u.jsx)("p",{children:"Page not found!"})})}}}]);
+//# sourceMappingURL=600.1a62996f.chunk.js.map
